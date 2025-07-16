9bc2f2157364bb0874f95b7fd28ae7f56a0e8bb9
morakot maneelart <dev.morakot@gmail.com>
2025-07-16 12:57:21 +0700

    fix error message

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,13 +8,25 @@ export const register = async (req, res) => {
   try {
     const { email, password } = req.body;
     const existing = await User.findOne({ email });
-    if (existing) return res.status(400).json({ msg: 'Email ซ้ำ' });
+    if (existing) return res.status(400).json({
+      success: false,
+      error: {
+        code: "DUPLICATE_EMAIL",
+        message: "อีเมลซ่ำ"
+      } 
+    });
 
     const user = await User.create({ email, password });
     const token = signToken(user._id);
     res.status(201).json({ token });
   } catch (e) {
-    res.status(500).json({ msg: 'Server error' });
+    res.status(500).json({ 
+      success: false,
+      error: {
+        code: "SERVER_ERROR",
+        message: "เกิดข้อผิดพลาดในการเชื่อมต่อเซิร์ฟเวอร์"
+      } 
+     });
   }
 };
 
@@ -23,12 +35,24 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user || !(await user.comparePassword(password)))
-      return res.status(400).json({ msg: 'อีเมลหรือรหัสผ่านไม่ถูกต้อง' });
+      return res.status(400).json({ 
+      success: false,
+      error: {
+        code: "AUTH_INVALID",
+        message: "อีเมลหรือรหัสผ่านไม่ถูกต้อง"
+      } 
+    });
 
     const token = signToken(user._id);
     res.json({ token });
   } catch (e) {
-    res.status(500).json({ msg: 'Server error' });
+    res.status(500).json({ 
+      success: false,
+      error: {
+        code: "SERVER_ERROR",
+        message: "เกิดข้อผิดพลาดในการเชื่อมต่อเซิร์ฟเวอร์"
+      } 
+     });
   }
 };
 
