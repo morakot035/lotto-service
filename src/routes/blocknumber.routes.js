67f9d5e423c76af847ceabdd0fd9c9608e0b72ed
morakot maneelart <dev.morakot@gmail.com>
2025-07-17 12:56:14 +0700

    fix routes blocknumber

--- src/routes/blocknumber.routes.js
+++ src/routes/blocknumber.routes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import {
-  getBuyers,
-  createBuyer,
-  updateBuyer,
-  deleteBuyer,
-} from '../controllers/buyer.controller.js';
+  getBlockNumber,
+  createBlockNumber,
+  updateBlockNumber,
+  deleteBlockNumber,
+} from '../controllers/blocknumber.conntroller.js';
 
 const router = express.Router();
 
-router.get('/', getBuyers);
-router.post('/', createBuyer);
-router.put('/:id', updateBuyer);
-router.delete('/:id', deleteBuyer);
+router.get('/', getBlockNumber);
+router.post('/', createBlockNumber);
+router.put('/:id', updateBlockNumber);
+router.delete('/:id', deleteBlockNumber);
 
 export default router;
