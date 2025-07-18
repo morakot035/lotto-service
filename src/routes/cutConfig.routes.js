bf9b1e6c5f4f3783266dd7f28159a9062da6add0
morakot maneelart <dev.morakot@gmail.com>
2025-07-18 13:57:46 +0700

    fix bug

--- src/routes/cutConfig.routes.js
+++ src/routes/cutConfig.routes.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const {
+import express from "express";
+import {
   saveCutConfig,
   getCutConfig,
-} = require("../controllers/cutConfig.controller");
-const verifyToken = require("../middlewares/verifyToken");
+} from "../controllers/cutConfig.controller";
+import { verifyToken } from "../middlewares/verifyToken.js";
+
+const router = express.Router();
 
 router.get("/cut-config", verifyToken, getCutConfig);
 router.post("/cut-config", verifyToken, saveCutConfig);
 
-module.exports = router;
+export default router;
