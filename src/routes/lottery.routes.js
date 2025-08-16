9d7cf45754778a297b8f045509fa272405167b50
morakot maneelart <dev.morakot@gmail.com>
2025-08-16 20:30:21 +0700

    test

--- src/routes/lottery.routes.js
+++ src/routes/lottery.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import lotteryController from "../controllers/lottery.controller.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
 // ผูก controller กับ route
-router.use("/", lotteryController);
+router.use("/", verifyToken, lotteryController);
 
 export default router;
